f1429160a26861ad9f987f582dd2027c707ac510
AmirHossein U3Fi <58352539+ahu3fi@users.noreply.github.com>
2024-07-06 03:15:47 +0330

    Final

--- Ipv4Scanner.py
+++ Ipv4Scanner.py
@@ -1,20 +1,122 @@
 import random
-from ping3 import ping
 import socket
+import json
+import os
 
-# Define IP address ranges
-ipv4_ranges = [
-    "162.159.192.",
-    "162.159.193.",
-    "162.159.195.",
-    "188.114.96.",
-    "188.114.97.",
-    "188.114.98.",
-    "188.114.99."
-]
+try:
+    from ping3 import ping
+except:
+    print("Please Install 'ping3' module...")
+    input()
 
-# Function to generate random IPv4 addresses
+def load_settings():
+    settings = {
+        "number_of_ip_scans": 100,
+        "show_best_ipv4_count": 5,
+    }
+    if os.path.exists('settings.json'):
+        with open('settings.json', 'r') as file:
+            settings = json.load(file)
+    return settings
+
+def save_settings(settings):
+    with open('settings.json', 'w') as file:
+        json.dump(settings, file, indent=4)
+
+def main():
+    global settings
+    settings = load_settings()
+    show_main_menu(settings)
+
+def show_main_menu(settings):
+    clear()
+    print("[1] Scan IPv4")
+    print("[2] Settings")
+    print("[3] Github (ahu3fi)")
+    print("[4] Exit")
+
+    option = input("Select an option: ")
+    if option == '1':
+        clear()
+        generate_ipv4(settings)
+    elif option == '2':
+        handle_settings(settings)
+    elif option == '3':
+        open_github()
+    elif option == '4':
+        exit()
+    else:
+        input("Invalid option! Press Enter to try again...")
+        show_main_menu(settings)
+
+def handle_settings(settings):
+    clear()
+    print("[1] Number of Ip Scans =", settings['number_of_ip_scans'])
+    print("[2] Show Best IPv4 by ping =", settings['show_best_ipv4_count'])
+    print("[3] Back")
+
+    option = input("Select an option: ")
+    if option == '1':
+        change_number_of_ip_scans()
+    elif option == '2':
+        change_show_best_ipv4_count()
+    elif option == '3':
+        show_main_menu(settings)
+        
+    else:
+        input("Invalid option! Press Enter to try again...")
+        handle_settings(settings)
+
+def change_number_of_ip_scans():
+    clear()
+    print("Enter the number of IP scans: ")
+    while True:
+        try:
+            num_scans = int(input("Number of IP scans: ").strip())
+            settings['number_of_ip_scans'] = num_scans
+            save_settings(settings)
+            break
+        except ValueError:
+            input("Invalid input! Please enter a number. Press Enter to try again...")
+            break
+    handle_settings(settings)
+
+def change_show_best_ipv4_count():
+    clear()
+    print("Enter the number of best IPv4 to show:")
+    while True:
+        try:
+            count = int(input("Number of best IPv4 to show: ").strip())
+            if 1 <= count <= settings['number_of_ip_scans']:
+                settings['show_best_ipv4_count'] = count
+                save_settings(settings)
+                break
+            else:
+                input(f"Number out of range! Please enter a number between 1 and {settings['number_of_ip_scans']}. Press Enter to try again...")
+                continue
+        except ValueError:
+            input("Invalid input! Please enter a number. Press Enter to try again...")
+            break
+    handle_settings(settings)
+    
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def open_github():
+    import webbrowser
+    webbrowser.open_new_tab('https://github.com/ahu3fi')
+    show_main_menu(settings)
+
 def generate_ipv4_list(count):
+    ipv4_ranges = [
+        "162.159.192.",
+        "162.159.193.",
+        "162.159.195.",
+        "188.114.96.",
+        "188.114.97.",
+        "188.114.98.",
+        "188.114.99."
+    ]
     ip_list = []
     while len(ip_list) < count:
         ip_range = random.choice(ipv4_ranges)
@@ -33,8 +135,8 @@ def is_valid_ipv4(ip):
         return None
     return None
 
-def main():
-    iplist = 100  # Number of IP addresses to generate
+def generate_ipv4(settings):
+    iplist = settings['number_of_ip_scans']
     unique_ips = generate_ipv4_list(iplist)
     
     ip_ping_times = {}
@@ -45,13 +147,16 @@ def main():
             print(f"{ip} with ping: {ping_time}ms")
     
     if ip_ping_times:
-        # Sort IPs by ping time and get the 5 best
-        best_ips = sorted(ip_ping_times, key=ip_ping_times.get)[:5]
-        print("\nBest 5 IPv4 addresses with lowest ping times:")
+        clear()
+        best_ips = sorted(ip_ping_times, key=ip_ping_times.get)[:settings['show_best_ipv4_count']]
+        print(f"\nBest {settings['show_best_ipv4_count']} IPv4 addresses with lowest ping times:")
         for ip in best_ips:
             print(f"{ip} - Ping: {ip_ping_times[ip]}ms")
     else:
         print("No valid IPv4 addresses found.")
+
+    input("Press Enter ... ")
+    show_main_menu(settings)
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Ipv4 Scanner
- 
+
+# IPv4 Scanner
+
+This is a python script that allows you to find version 4 IPs and sort the best ones by latency.
+
+* Python version : [3.12.4](https://www.python.org/downloads/release/python-3124/)
+[![python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)](https://www.python.org/downloads/release/python-3124)
+
+## Installation
+
+Use the package manager [pip](https://pypi.org/project/ping3) to install ping3.
+
+```bash
+pip install ping3
+```
+
+### 💡 Hint
+
+⚙ Using the settings panel, you can specify the number of IPs you want to be scanned (default: 100).
+
+⏳ You can also change the number of IPs you want to be sorted by lowest ping time (default: 5).
+
+📝 Or open the settings.json file and make the changes manually.
+## 🔗 My Telegram Channel
+[![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/ahu3fi_channel)
